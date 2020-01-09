441d8880faae35e17b926fabf3597707a0224b35
Abhinav Ramachandran <geckods@gmail.com>
2020-01-09 15:09:42 +0530

    Made HQ broadcast it's location on round 2.

--- src/firstbot/HQBot.java
+++ src/firstbot/HQBot.java
@@ -12,7 +12,24 @@ public strictfp class HQBot extends Globals
 
     public static void run(RobotController rc) throws GameActionException
     {
-		
+		/*The following code makes the HQ broadcast it's own location*/
+		if(Globals.roundNum==1){
+			int initialArr[] = new int[12];
+			initialArr[0] = getCommsNum(Globals.myType,Globals.currentPos);
+			sendComs(arr,0);
+
+			/*To read this, the code is as follows:
+				int newarr[][]=getComms(2);
+				for(int i=0;i<newarr.length;i++){
+					//this loop iterates over all messages of round 2 (since we don't know which one is ours)
+					//if it's an enemy message, the output will be COW at (0,0)
+					getLocationFromInt(newarr[i][0]).rt //RobotType HQ
+					getLocationFromInt(newarr[i][0]).loc.x //integer HQ x position
+					getLocationFromInt(newarr[i][0]).loc.y //integer HQ y position
+				}
+			*/
+
+   		}
     }
 
     static Boolean buildMiner() throws GameActionException
