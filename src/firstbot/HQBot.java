bd113e4291821b924d9ab644feec64acfd89b490
Rahul Prabhu <cupertinorp@gmail.com>
2020-01-09 14:56:00 +0530

    I hate git

--- src/firstbot/HQBot.java
+++ src/firstbot/HQBot.java
@@ -7,36 +7,12 @@ import battlecode.common.*;
  * Produces: Miners
  * Has a built in gun and refinery.
  */
-public strictfp class HQBot
+public strictfp class HQBot extends Globals
 {	
-
-    static Direction[] directions = {Direction.NORTH, Direction.NORTHEAST, Direction.EAST, Direction.SOUTHEAST, Direction.SOUTH, Direction.SOUTHWEST, Direction.WEST, Direction.NORTHWEST};
-    
-    static int numberOfMinerCreated = 0;
 
     public static void run(RobotController rc) throws GameActionException
     {
-     	//strategy 
-     	//1. build miners .. 
-     	//2. miners build refineries and communicate for more miners
-     	//3. wall ourselves in.
-		Globals.rc = rc;     	
-     	//get messages
-     	transaction = rc.getBlock(rc.getRoundNum());
-     	if(numberOfMinerCreated < 3){
-     		if(buildUnit()){
-     			numberOfMinerCreated++;
-     		}
-     	}
-
-     	//if not mining any resources , build more miners
-
-     	if(rc.getRoundNum() > 100 && rc.getRoundNum()/70 >= numberOfMinerCreated-3){
-     		if(buildMiner()){
-     			numberOfMinerCreated++;
-     		}
-     	}
-
+		
     }
 
     static Boolean buildMiner() throws GameActionException
--- src/firstbot/LandscaperBot.java
+++ src/firstbot/LandscaperBot.java
@@ -25,9 +25,9 @@ public strictfp class LandscaperBot extends Globals
     }
 
     static void fortifyBase(){
-		short x[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
-	    short y[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
-	    Direction directions[8] = {Direction.SOUTHWEST, Direction.SOUTH, Direction.SOUTHEAST, Direction.EAST, Direction.NORTHEAST, Direction.NORTH, Direction.NORTHWEST, Direction.WEST};
+		short x[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+	    short y[] = {-1, -1, -1, 0, 1, 1, 1, 0};
+	    Direction directions[] = {Direction.SOUTHWEST, Direction.SOUTH, Direction.SOUTHEAST, Direction.EAST, Direction.NORTHEAST, Direction.NORTH, Direction.NORTHWEST, Direction.WEST};
     	if(dumping){
     		if(rc.getDirtCarrying()){
     			rc.depositDirection(dumpingTo);
@@ -39,11 +39,11 @@ public strictfp class LandscaperBot extends Globals
     	else{
 	    	if(rc.getDirtCarrying() < 3){
 	    		for(int t = 0; t < 8; t++){
-	    			RobotInfo baseInfo = rc.senseRobot(new MapLocation(currentPos.x + x[t], currentPos.y + y[t]))
+	    			RobotInfo baseInfo = rc.senseRobot(new MapLocation(currentPos.x + x[t], currentPos.y + y[t]));
 	    			if(baseID == baseInfo.ID){
 	    				t += 3;
 	    				t %= 8;
-	    				for(int i = 0; i < 3; i++;){
+	    				for(int i = 0; i < 3; i++){
 	    					if(rc.canDigDirt(directions[t])){
 	    						rc.digDirt(directions[t]);
 	    						break;
@@ -56,10 +56,10 @@ public strictfp class LandscaperBot extends Globals
 	    	}
 	    	else{
 	    		for(int t = 0; t < 8; t++){
-	    			RobotInfo baseInfo = rc.senseRobot(new MapLocation(currentPos.x + x[t], currentPos.y + y[t]))
+	    			RobotInfo baseInfo = rc.senseRobot(new MapLocation(currentPos.x + x[t], currentPos.y + y[t]));
 	    			if(baseID == baseInfo.ID){
 	    				t++;
-	    				t %/= 8; 
+	    				t %= 8; 
 	    				if(rc.senseElevation(new MapLocation(currentPos.x, currentPos.y) >= rc.senseElevation(currentPos.x + x[t], currentPos.y + y[t]))){
 	    					dumping = true;
 	    					dumpingTo = directions[t];
