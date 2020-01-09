bc60a731902248df453221055264a1ebbff9fbee
AryamanBhagat <aryamanbhagat200@gmail.com>
2020-01-09 16:41:01 +0530

    NetGunBot code sans cow handling and comms

--- src/firstbot/NetGunBot.java
+++ src/firstbot/NetGunBot.java
@@ -11,28 +11,35 @@ import battlecode.common.*;
  */
 public strictfp class NetGunBot extends Globals
 {
-    public static void run(RobotController rc)
+    public static void run(RobotController rc) throws GameActionException
     {
-    	RobotInfo nearbyUnits = rc.senseNearbyRobots();
-    	if(nearbyDrones.length() > 0){
+    	RobotInfo nearbyUnits[] = rc.senseNearbyRobots();
+    	if(nearbyUnits != null){
 	    	    //shoot nearest drone
+    			RobotInfo nearbyEnemyUnits[] = null;
     			if(rc.getTeam() == Team.A){
-    				RobotInfo nearbyEnemyUnits = rc.senseNearbyRobots(rc.getCurrentSensorRadiusSquared(), Team.B);
+    				nearbyEnemyUnits = rc.senseNearbyRobots(rc.getCurrentSensorRadiusSquared(), Team.B);
     			}
     			else{
-    			RobotInfo nearbyEnemyUnits = rc.senseNearbyRobots(rc.getCurrentSensorRadiusSquared(), Team.A);
+    				nearbyEnemyUnits = rc.senseNearbyRobots(rc.getCurrentSensorRadiusSquared(), Team.A);
     			}
-    			if(nearbyUnits.length() > 0){
-    				int nearestDroneID = nearbyEnemyUnits[0].ID;
+    			if(nearbyUnits != null){
+    				int nearestDroneID = nearbyEnemyUnits[0].getID();
     				int nearestDroneDist = 64*64;
-    				for(int i = 0; i < nearbyEnemyUnits.length()){
-    					if(nearbyEnemyUnits[i].RobotType == RobotType.DELIVERY_DRONE && nearbyEnemyUnits[]){
-    						nears
+    				for(int i = 0; i < nearbyEnemyUnits.length; i++){
+    					int distance = (nearbyEnemyUnits[i].getLocation()).distanceSquaredTo(rc.getLocation());
+    					if(nearbyEnemyUnits[i].getType() == RobotType.DELIVERY_DRONE && distance < nearestDroneDist){
+    						nearestDroneID = nearbyEnemyUnits[i].getID();
+    						nearestDroneDist = distance;
     					}
+    				}
+    				if(rc.canShootUnit(nearestDroneID)){
+    					rc.shootUnit(nearestDroneID);
     				}
     			}
 
 	    		//communicate cows?
+
 	    }
 	}
 
