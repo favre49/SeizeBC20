cac6eab0d2bd4cca5bd0935356261865f7169c57
Abhinav Ramachandran <geckods@gmail.com>
2020-01-09 16:15:35 +0530

    Created basic HQBot behavior that simply broadcasts it's own location on turn 1 and reads it out from the blockchain on subsequent turns.

--- src/firstbot/HQBot.java
+++ src/firstbot/HQBot.java
@@ -13,22 +13,22 @@ public strictfp class HQBot extends Globals
     public static void run(RobotController rc) throws GameActionException
     {
 		/*The following code makes the HQ broadcast it's own location*/
-		if(Globals.roundNum==1){
+		if(roundNum==1){
 			int initialArr[] = new int[12];
-			initialArr[0] = getCommsNum(Globals.myType,Globals.currentPos);
-			sendComs(arr,0);
-
-			/*To read this, the code is as follows:
-				int newarr[][]=getComms(2);
+			initialArr[0] = Communications.getCommsNum(Globals.myObjectType,Globals.currentPos);
+			System.out.print(Communications.sendComs(initialArr,0));
+   		}
+   		else if (roundNum>1){
+   			/*To read this, the code is as follows:*/
+				int newarr[][]=Communications.getComms(1);
 				for(int i=0;i<newarr.length;i++){
 					//this loop iterates over all messages of round 2 (since we don't know which one is ours)
 					//if it's an enemy message, the output will be COW at (0,0)
-					getLocationFromInt(newarr[i][0]).rt //RobotType HQ
-					getLocationFromInt(newarr[i][0]).loc.x //integer HQ x position
-					getLocationFromInt(newarr[i][0]).loc.y //integer HQ y position
+					System.out.println(Communications.getLocationFromInt(newarr[i][0]).rt); //RobotType HQ
+					System.out.println(Communications.getLocationFromInt(newarr[i][0]).loc.x); //integer HQ x position
+					System.out.println(Communications.getLocationFromInt(newarr[i][0]).loc.y); //integer HQ y position
 				}
-			*/
-
+			/**/
    		}
     }
 
