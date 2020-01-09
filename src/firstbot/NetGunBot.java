94f955a96f22e23b1b654de5a0879aa07ab711c9
AryamanBhagat <aryamanbhagat200@gmail.com>
2020-01-09 15:47:51 +0530

    FINAL SOLUTION LANDSCAPER

--- src/firstbot/NetGunBot.java
+++ src/firstbot/NetGunBot.java
@@ -13,6 +13,27 @@ public strictfp class NetGunBot extends Globals
 {
     public static void run(RobotController rc)
     {
-        
-    }
+    	RobotInfo nearbyUnits = rc.senseNearbyRobots();
+    	if(nearbyDrones.length() > 0){
+	    	    //shoot nearest drone
+    			if(rc.getTeam() == Team.A){
+    				RobotInfo nearbyEnemyUnits = rc.senseNearbyRobots(rc.getCurrentSensorRadiusSquared(), Team.B);
+    			}
+    			else{
+    			RobotInfo nearbyEnemyUnits = rc.senseNearbyRobots(rc.getCurrentSensorRadiusSquared(), Team.A);
+    			}
+    			if(nearbyUnits.length() > 0){
+    				int nearestDroneID = nearbyEnemyUnits[0].ID;
+    				int nearestDroneDist = 64*64;
+    				for(int i = 0; i < nearbyEnemyUnits.length()){
+    					if(nearbyEnemyUnits[i].RobotType == RobotType.DELIVERY_DRONE && nearbyEnemyUnits[]){
+    						nears
+    					}
+    				}
+    			}
+
+	    		//communicate cows?
+	    }
+	}
+
 }
