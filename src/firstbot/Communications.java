486493f7b6a3328b3e1d04a8a45597bfab94b480
Abhinav Ramachandran <geckods@gmail.com>
2020-01-10 16:25:28 +0530

    Added untested Hamming Codes to Comms.

--- src/firstbot/Communications.java
+++ src/firstbot/Communications.java
@@ -3,37 +3,110 @@ import battlecode.common.*;
 
 public strictfp class Communications extends Globals{
 
-	public static boolean sendComs(int[] data, int bidamount/*message, bid amount*/) throws GameActionException {
+	public static int hamming18to24(int data){
+		/*https://www.xilinx.com/support/documentation/application_notes/xapp383.pdf*/
+
+		//data is now an integer where only the last 18 bits are relevant
+		int newData=0;
+
+		//putting data in newData
+		newData|=((data&(1<<0))<<2);
+		newData|=((data&(1<<1))<<3);
+		newData|=((data&(1<<2))<<3);
+		newData|=((data&(1<<3))<<3);
+		newData|=((data&(1<<4))<<4);
+		newData|=((data&(1<<5))<<4);
+		newData|=((data&(1<<6))<<4);
+		newData|=((data&(1<<7))<<4);
+		newData|=((data&(1<<8))<<4);
+		newData|=((data&(1<<9))<<4);
+		newData|=((data&(1<<10))<<4);
+		newData|=((data&(1<<11))<<5);
+		newData|=((data&(1<<12))<<5);
+		newData|=((data&(1<<13))<<5);
+		newData|=((data&(1<<14))<<5);
+		newData|=((data&(1<<15))<<5);
+		newData|=((data&(1<<16))<<5);
+		newData|=((data&(1<<17))<<5);
+
+		//building parity bits
+		int r1 = ((newData>>2)&1)^((newData>>4)&1)^((newData>>6)&1)^((newData>>8)&1)^((newData>>10)&1)^((newData>>12)&1)^((newData>>14)&1)^((newData>>16)&1)^((newData>>18)&1)^((newData>>20)&1)^((newData>>22)&1);
+		int r2 = ((newData>>2)&1)^((newData>>5)&1)^((newData>>6)&1)^((newData>>9)&1)^((newData>>10)&1)^((newData>>13)&1)^((newData>>14)&1)^((newData>>17)&1)^((newData>>18)&1)^((newData>>21)&1)^((newData>>22)&1);
+		int r3 = ((newData>>4)&1)^((newData>>5)&1)^((newData>>6)&1)^((newData>>11)&1)^((newData>>12)&1)^((newData>>13)&1)^((newData>>14)&1)^((newData>>19)&1)^((newData>>20)&1)^((newData>>21)&1)^((newData>>22)&1);
+		int r4 = ((newData>>8)&1)^((newData>>9)&1)^((newData>>10)&1)^((newData>>11)&1)^((newData>>12)&1)^((newData>>13)&1)^((newData>>14)&1);
+		int r5 = ((newData>>16)&1)^((newData>>17)&1)^((newData>>18)&1)^((newData>>19)&1)^((newData>>20)&1)^((newData>>21)&1)^((newData>>22)&1);
+
+		int finalparity = ((newData>>0)&1)^((newData>>1)&1)^((newData>>2)&1)^((newData>>3)&1)^((newData>>4)&1)^((newData>>5)&1)^((newData>>6)&1)^((newData>>7)&1)^((newData>>8)&1)^((newData>>9)&1)^((newData>>10)&1)^((newData>>11)&1)^((newData>>12)&1)^((newData>>13)&1)^((newData>>14)&1)^((newData>>15)&1)^((newData>>16)&1)^((newData>>17)&1)^((newData>>18)&1)^((newData>>19)&1)^((newData>>20)&1)^((newData>>21)&1)^((newData>>22)&1);
+
+		//adding parity bits to newData
+		newData|=r1;
+		newData|=r2<<1;
+		newData|=r3<<3;
+		newData|=r4<<7;
+		newData|=r5<<15;
+
+		newData|=finalparity<<23;
+		return newData;
+	}
+
+	public static boolean isCorrect(int data){
+		int r1 = ((data>>2)&1)^((data>>4)&1)^((data>>6)&1)^((data>>8)&1)^((data>>10)&1)^((data>>12)&1)^((data>>14)&1)^((data>>16)&1)^((data>>18)&1)^((data>>20)&1)^((data>>22)&1);
+		int r2 = ((data>>2)&1)^((data>>5)&1)^((data>>6)&1)^((data>>9)&1)^((data>>10)&1)^((data>>13)&1)^((data>>14)&1)^((data>>17)&1)^((data>>18)&1)^((data>>21)&1)^((data>>22)&1);
+		int r3 = ((data>>4)&1)^((data>>5)&1)^((data>>6)&1)^((data>>11)&1)^((data>>12)&1)^((data>>13)&1)^((data>>14)&1)^((data>>19)&1)^((data>>20)&1)^((data>>21)&1)^((data>>22)&1);
+		int r4 = ((data>>8)&1)^((data>>9)&1)^((data>>10)&1)^((data>>11)&1)^((data>>12)&1)^((data>>13)&1)^((data>>14)&1);
+		int r5 = ((data>>16)&1)^((data>>17)&1)^((data>>18)&1)^((data>>19)&1)^((data>>20)&1)^((data>>21)&1)^((data>>22)&1);
+
+		int syndrome=r1^r2^r3^r4^r5^(data&1)^((data>>1)&1)^((data>>3)&1)^((data>>7)&1)^((data>>15)&1);
+		if(syndrome==0 && (data>>23)==0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	public static int unhamming24to18(int data){
+		int newData=0;
+		newData|=((data&(1<<2))>>2);
+		newData|=((data&(1<<4))>>3);
+		newData|=((data&(1<<5))>>3);
+		newData|=((data&(1<<6))>>3);
+		newData|=((data&(1<<8))>>4);
+		newData|=((data&(1<<9))>>4);
+		newData|=((data&(1<<10))>>4);
+		newData|=((data&(1<<11))>>4);
+		newData|=((data&(1<<12))>>4);
+		newData|=((data&(1<<13))>>4);
+		newData|=((data&(1<<14))>>4);
+		newData|=((data&(1<<16))>>5);
+		newData|=((data&(1<<17))>>5);
+		newData|=((data&(1<<18))>>5);
+		newData|=((data&(1<<19))>>5);
+		newData|=((data&(1<<20))>>5);
+		newData|=((data&(1<<21))>>5);
+		newData|=((data&(1<<22))>>5);
+
+		return newData;
+	}
+
+	public static int[] encode(int[] data){
+		//takes int[9], adds hamming to each, then given int
+		int[] hammingData = new int[9];
+		for(int i=0;i<9;i++){
+			hammingData[i]=hamming18to24(data[i]);
+		}
+
 		int[] message = new int[7];
-
-		/*
-		The XOR values for encryption/decryption
-		-903849746
-		-172817894
-		293009196
-		25721274
-		205865773
-		-1561017189
-		2092647876
-		*/
-
+		message[0]|=(38<<24);	
 		//first 8 bits for key
 		//the 8 bit key is 38
-
-		message[0]|=(38<<24);
-
-		//first 8 bits of message is reserved
-
-		//224-8=216 bits remaining
-		//each message stores 12 packets of 18 bits each and one packet of 12 bits
-
-		//one bit for whether actual data or key data stored
-
+		
 		for(int i=0;i<216;i++){
-			int whichdata = i/18;
-			int whichbit = i%18;
+			int whichdata = i/24;
+			int whichbit = i%24;
 
-			int sourcebit = ((data[whichdata]&(1<<(17-whichbit)))>0)?1:0;
+			int sourcebit = ((data[whichdata]&(1<<(23-whichbit)))>0)?1:0;
+
 
 			int targetdata = (i+8)/32;
 			int targetbit = (i+8)%32;
@@ -48,20 +121,21 @@ public strictfp class Communications extends Globals{
 		message[4]^=205865773;
 		message[5]^=-1561017189;
 		message[6]^=2092647876;
-
-		if (rc.canSubmitTransaction(message, bidamount)){
-			rc.submitTransaction(message, bidamount);
-			return true;
-		}
-		else{
-			return false;
-		}
-
+		/*
+		The XOR values for encryption/decryption
+		-903849746
+		-172817894
+		293009196
+		25721274
+		205865773
+		-1561017189
+		2092647876
+		*/
+		return message;
 	}
 
-
 	public static int[] decode(Transaction messageTransaction) throws GameActionException{
-		int[] decoded = new int[12];
+		int[] decoded = new int[9];
 
 		int[] message = messageTransaction.getMessage();
 
@@ -82,20 +156,40 @@ public strictfp class Communications extends Globals{
 		}
 
 		for(int i=0;i<216;i++){
-			int whichdata = i/18;
-			int whichbit = i%18;
+			int whichdata = i/24;
+			int whichbit = i%24;
 
 			int messagedata = (i+8)/32;
 			int messagebit = (i+8)%32;
 
 			int sourcebit = ((message[messagedata]&(1<<31-messagebit))>0)?1:0;
-			decoded[whichdata]|=sourcebit<<(17-whichbit);
+			decoded[whichdata]|=sourcebit<<(23-whichbit);
 		}
 
+		int[] hammingChecked=new int[9];
+		for(int i=0;i<12;i++){
+			if(isCorrect(decoded[i])){
+				hammingChecked[i]=unhamming24to18(decoded[i]);
+			}
+		}
 
-		return decoded;
+		return hammingChecked;
+	}	
+
+
+	public static boolean sendComs(int[] data, int bidamount/*message, bid amount*/) throws GameActionException {
+
+		int message[] = encode(data);
+
+		if (rc.canSubmitTransaction(message, bidamount)){
+			rc.submitTransaction(message, bidamount);
+			return true;
+		}
+		else{
+			return false;
+		}
+
 	}
-
 
 
 	public static int[][] getComms(int roundno) throws GameActionException{
